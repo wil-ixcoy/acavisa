9c991d92ae8a2e732bda742eb1ab38745d3d8b91
Wiliams Ixcoy <ixcoywiliams@gmail.com>
2025-03-06 21:33:12 -0600

    add promotions component and add in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,8 @@ import CategoryCard from "@/components/ladingpage/categoryCard";
 import Header from "@/components/ladingpage/header";
 import HelpComponent from "@/components/ladingpage/helpComponent";
 import NavBar from "@/components/ladingpage/navBar";
+import PromotionsSection from "@/components/ladingpage/promotions";
+
 export default function Home() {
   return (
     <div>
@@ -13,6 +15,7 @@ export default function Home() {
       <Ads/>
       <HelpComponent/>
       <CategoryCard/>
+      <PromotionsSection/>
     </div>
   );
 }
