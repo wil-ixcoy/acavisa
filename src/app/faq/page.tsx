fa11ec446a23e8db684d81a4b31e35956d86daf8
Wiliams Ixcoy <ixcoywiliams@gmail.com>
2025-03-11 09:21:07 -0600

    add: faq page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,16 +1,72 @@
-
 import Header from "@/components/ladingpage/header";
 import NavBar from "@/components/ladingpage/navBar";
-
 import Footer from "@/components/ladingpage/footer";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import Title from "@/components/ladingpage/title";
+
 export default function Faq() {
+  const faqData = [
+    {
+      question: "¿Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      question: "¿Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      question: "¿Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      question: "¿Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      question: "¿Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ];
+
   return (
     <div>
       <header>
         <Header />
         <NavBar />
       </header>
+
+      <main className="max-w-3xl mx-auto py-10 px-6">
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <Title>
+            <p>PREGUNTAS FRECUENTES</p>
+          </Title>
+
+          <Accordion type="single" collapsible className="my-8">
+            {faqData.map((item, index) => (
+              <AccordionItem value={`item-${index}`} key={index}>
+                <AccordionTrigger className="text-green-800 font-bold cursor-pointer transition-all duration-300 hover:text-green-700 p-3 rounded-lg">
+                  {item.question}
+                </AccordionTrigger>
+                <AccordionContent className="transition-all duration-300 ease-in-out">
+                  <p className="text-green-700">{item.answer}</p>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
+      </main>
 
       <Footer />
     </div>
