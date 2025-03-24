4aac30175e771a25bddd66ed5a9ec9d6b81674bf
Erick Marroquín <erick.marro@outlook.com>
2025-03-24 10:05:03 -0600

    api routes updated 2

--- src/app/api/categories/[categoryId]/route.ts
+++ src/app/api/categories/[categoryId]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 
-export async function GET(request: Request, { params }: { params: { categoryId: string } }) {
+export async function GET(request: Request, { params }: any) {
   const supabase = await createClient();
   const { categoryId } = params;
 
