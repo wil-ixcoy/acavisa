ca4acbaf04bf690a6dd02c2fb6d49782047b8a07
Wiliams Ixcoy <ixcoywiliams@gmail.com>
2025-03-25 18:37:16 -0600

    update: add acavisa name and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const countries = [
+    { name: "EL SALVADOR", flag: "/countries/sv.png" },
+    { name: "HONDURAS", flag: "/countries/hn.png" },
+    { name: "NICARAGUA", flag: "/countries/ni.png" },
+  ];
+
   return (
     <div className="h-screen bg-gray-100">
       <div
@@ -19,69 +26,28 @@ export default function Home() {
       <main className="flex flex-col justify-center items-center">
         <div className="mt-10 w-80 text-center">
           <h2>
-            <span className="text-3xl font-bold text-secondary">
-              SELECCIONAR
-            </span>{" "}
+            <span className="text-3xl font-bold text-secondary">SELECCIONAR</span>{" "}
             <span className="text-3xl font-bold text-black-700">PAÍS</span>
           </h2>
 
-          <hr className="w-[100%] my-4 border-green-700 mx-auto border-1" />
+          <hr className="w-full my-4 border-green-700 mx-auto border-1" />
+
           <div className="flex flex-col gap-4 mt-4 items-center justify-center">
-            <Button className="bg-secondary text-white flex justify-between w-full h-12 py-2 px-4 rounded-lg hover:cursor-pointer">
-              <Image
-                src="/countries/sv.png"
-                alt="sv"
-                width={30}
-                height={30}
-                className="object-contain"
-              />
-              <span className="flex items-center gap-2 text-lg">
-                EL SALVADOR
-              </span>
-              <Image
-                src="/icons/arrow-select-country.png"
-                alt="ACAVISA"
-                width={20}
-                height={20}
-                className="object-contain filter"
-              />
-            </Button>
-
-            <Button className="bg-secondary text-white flex justify-between w-full h-12 py-2 px-4 rounded-lg hover:cursor-pointer">
-              <Image
-                src="/countries/hn.png"
-                alt="hn"
-                width={30}
-                height={30}
-                className="object-contain"
-              />
-              <span className="flex items-center gap-2 text-lg">HONDURAS</span>
-              <Image
-                src="/icons/arrow-select-country.png"
-                alt="ACAVISA"
-                width={20}
-                height={20}
-                className="object-contain filter"
-              />{" "}
-            </Button>
-
-            <Button className="bg-secondary text-white flex justify-between w-full h-12 py-2 px-4 rounded-lg hover:cursor-pointer">
-              <Image
-                src="/countries/ni.png"
-                alt="ni"
-                width={30}
-                height={30}
-                className="object-contain"
-              />
-              <span className="flex items-center gap-2 text-lg">NICARAGUA</span>
-              <Image
-                src="/icons/arrow-select-country.png"
-                alt="ACAVISA"
-                width={20}
-                height={20}
-                className="object-contain filter"
-              />{" "}
-            </Button>
+            {countries.map(({ name, flag }, index) => (
+              <Link key={index} href="/home">
+                <Button className="bg-secondary text-white flex justify-between w-60 h-12 py-2 px-4 rounded-lg transition-transform transform hover:scale-105 cursor-pointer">
+                  <Image src={flag} alt={name} width={30} height={30} className="object-contain" />
+                  <span className="flex items-center gap-2 text-lg">{name}</span>
+                  <Image
+                    src="/icons/arrow-select-country.png"
+                    alt="arrow"
+                    width={20}
+                    height={20}
+                    className="object-contain"
+                  />
+                </Button>
+              </Link>
+            ))}
           </div>
         </div>
       </main>
