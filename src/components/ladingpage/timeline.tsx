b62c31d58bee16c34224eb29129c904ff3dab349
Wiliams Ixcoy <ixcoywiliams@gmail.com>
2025-03-25 18:09:13 -0600

    update history page

--- src/components/ladingpage/timeline.tsx
+++ src/components/ladingpage/timeline.tsx
@@ -4,16 +4,16 @@ export default function Timeline({ children }: { children: React.ReactNode }) {
   const childrenArray = Children.toArray(children);
 
   return (
-    <div className="relative flex flex-col items-center">
-      <div className="absolute left-1/2 transform -translate-x-1/2 w-1 bg-gray-300 h-full"></div>
+    <div className="relative flex flex-col items-center w-full md:w-3/4 mx-auto px-4">
+      <div className="absolute left-1/2 transform -translate-x-1/2 w-1 bg-gray-300 h-full hidden md:block"></div>
 
       {childrenArray.map((child, index) => (
-        <div key={index} className="relative flex items-center w-full">
+        <div key={index} className="relative flex items-center w-full py-4">
           <div className="w-full flex items-center">
-            <div className="relative w-full">
+            <div className="relative w-full text-center md:text-left">
               {child}
 
-              <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4 h-4 rounded-full bg-gray-300 border border-white top-1/2"></div>
+              <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4 h-4 md:w-6 md:h-6 rounded-full bg-gray-300 hidden md:block border-2 border-white top-1/2"></div>
             </div>
           </div>
         </div>
